94424f95fa999923f1b5c845e2ae1ecbb2fdb9e3
Ridwan Rahman <ridwanrahman07@gmail.com>
2025-06-26 22:44:55 +1000

    Added black formatting

--- manage.py
+++ manage.py
@@ -3,14 +3,15 @@
 import os
 import sys
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_dep.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_dep.settings")
     current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.insert(0, os.path.join(current_path, 'src'))
+    sys.path.insert(0, os.path.join(current_path, "src"))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -22,5 +23,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
